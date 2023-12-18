b13bd252e6141d516e583bbf5280876d1dc3ae03
Sthefany Sousa <sthefanyssbastos@hotmail.com>
2023-12-18 09:53:21 -0300

    Class Repository_Produto²

--- uni/src/main/java/com/generation/uni/repository/ProdutoRepository.java
+++ uni/src/main/java/com/generation/uni/repository/ProdutoRepository.java
@@ -1,5 +1,12 @@
 package com.generation.uni.repository;
 
-public interface ProdutoRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.uni.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
